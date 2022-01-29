5c896a26451d964480547a9a9447dbf2bda75139
Aryan0509 <87501633+Aryan0509@users.noreply.github.com>
2022-01-29 15:33:00 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,38 +1,35 @@
-const express=require("express");
-const bodyParser=require("body-parser");
-const mongoose=require("mongoose");
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 const { stringify } = require("querystring");
 const { message } = require("statuses");
 const strict = require("assert/strict");
-const app=express();
-app.use(bodyParser.urlencoded({extended: true}));
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 mongoose.connect("mongodb+srv://admin-aryan2:test123@cluster0.5spc0.mongodb.net/contact");
-const schema={
+const schema = {
     email: String,
-    message:String
+    message: String
 }
-   const Contact=mongoose.model("Contact",schema); 
+const Contact = mongoose.model("Contact", schema);
 
-app.get("/",function(req,res)
-{
-    res.sendFile(__dirname+"/index.html");
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + "/index.html");
 })
-app.post("/",function(req,res)
-{   if(req.body.email!="")
-{
-      const contact=new Contact(
-    {
-        email:req.body.email,
-        message:req.body.message
+app.post("/", function (req, res) {
+    if (req.body.email != "") {
+        const contact = new Contact(
+            {
+                email: req.body.email,
+                message: req.body.message
+            }
+        );
+        contact.save();
     }
-);
-    contact.save();
-}
 
     res.redirect("/");
 })
-app.listen(process.env.PORT||3000,function()
-{
+app.listen(process.env.PORT || 3000, function () {
     console.log("server started");
 })
